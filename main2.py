e14e13f1fcf096cb89cefe613177c52e70b4383b
Nivesh-GitHub <89345698+Nivesh-GitHub@users.noreply.github.com>
2021-10-18 20:00:36 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,3 +1,4 @@
+#Imports
 import datetime as dt
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -9,14 +10,14 @@ import os
 
 
 
-
+#Start End
 start = dt.datetime(2000,1,1)
 end = dt.datetime(2021,12,31)
 
 
 
 
-
+#Yfiance
 df=  yfinance.download("FORCEMOT.BO", start, end) #Need yfiance
 df["Factor"] = df["Close"] / df["Adj Close"]
 print(df["Factor"].unique(), df["Factor"].count())
@@ -40,22 +41,11 @@ print(df_ohlc.head())
 
 
 
-df["SMA1"] = df['Close'].rolling(window=50).mean()
-df["SMA2"] = df['Close'].rolling(window=200).mean()
-
-plt.figure(figsize=(10,10))
-plt.plot(df['SMA1'], 'g--', label="SMA1")
-plt.plot(df['SMA2'], 'r--', label="SMA2")
-plt.plot(df['Close'], label="Close")
-plt.legend()
-plt.show()
 
 
 
 
-
-
-  
+#Charts  
 ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan = 1 )
 ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan = 1)
 
@@ -65,7 +55,7 @@ print(df.head())
 ax1.plot(df.index,df["Adj Close"])
 ax1.plot(df.index,df["100ma"])
 ax2.plot(df.index,df["100ma"])
-plt.show
+plt.show()
 
 
 
@@ -83,6 +73,4 @@ print(df.head())
 ax1.plot(df.index,df["Adj Close"])#Close of market
 ax1.plot(df.index,df["100ma"])
 ax2.plot(df.index,df["100ma"]) #ma stands for moving average
-plt.show
-
-#Thank you 
+plt.show()
